a91e3b1eff4442538d9b69cc8e62f684fed4eaa0
Jeff Kimbrough <40250282+jkimbrough0@users.noreply.github.com>
2019-01-15 08:47:34 -0800

    Create AppCaller.py

---
+++ SupervisedClass/AppCaller.py
@@ -0,0 +1,31 @@
+from __future__ import print_function, division
+from builtins import range, input
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+from util import get_data
+
+# make a prediction from your own server!
+# this could be coming from any client in a production sceanrio
+
+X, Y = get_data()
+N = len(Y)
+while True:
+    i = np.random.choice(N)
+    r = requests.post("http://localhost:8888/predict", data={'input': X[i]})
+    print("RESPONSE:")
+    print(r.content)
+    j = r.json()
+    print(j)
+    print("target:", Y[i])
+
+    plt.imshow(X[i].reshape(28, 28), cmap='gray')
+    plt.title("Target: %d, Prediction: %d" % (Y[i], j['prediction']))
+    plt.show()
+
+    response = input("Continue? (Y/n)\n")
+    if response in ('n', 'N'):
+        break
