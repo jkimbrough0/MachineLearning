0cbffc29b7ca436d1ca738c4bf2fb61c93a1ad7b
Jeff Kimbrough <40250282+jkimbrough0@users.noreply.github.com>
2019-01-15 08:23:27 -0800

    Create GenerateDataset1.py

---
+++ LinearRegression/GenerateDataset1.py
@@ -0,0 +1,15 @@
+# generates 1-dimensional data for linear regression analysis
+
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+import numpy as np
+
+N = 100
+with open('data_1d.csv', 'w') as f:
+    X = np.random.uniform(low=0, high=100, size=N)
+    Y = 2*X + 1 + np.random.normal(scale=5, size=N)
+    for i in range(N):
+        f.write("%s,%s\n" % (X[i], Y[i]))
